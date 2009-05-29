e217f5d080f08dcd51b67db57f239532b2479de9
Alexander Artemenko <svetlyak.40wt@gmail.com>
2009-05-29 16:50:26 +0400

    All code was moved to 'src' directory. Added setup.py, MANIFEST.in and ChangeLog.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'django-globals',
+    version = '0.1.0',
+    description = 'Very simple application, that allow to define a thread specific global variables.',
+    keywords = 'django apps',
+    license = 'New BSD License',
+    author = 'Alexander Artemenko',
+    author_email = 'svetlyak.40wt@gmail.com',
+    url = 'http://github.com/svetlyak40wt/django-globals/',
+    install_requires = [],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    package_dir = {'': 'src'},
+    packages = find_packages('src', exclude = ['example']),
+    include_package_data = True,
+)
+
+
