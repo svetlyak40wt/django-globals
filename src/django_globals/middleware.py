435df82c818cbbca68b666a630b82afab64bd4cb
twidi <s.angel@twidi.com>
2011-09-12 00:41:58 +0200

    set globals.user to None if there is no user, second try, better

--- src/django_globals/middleware.py
+++ src/django_globals/middleware.py
@@ -3,10 +3,7 @@ from django_globals import globals
 class Global(object):
     def process_request(self, request):
         globals.request = request
-        if hasattr(request, 'user'):
-            globals.user = request.user
-        else:
-            globals.user = None
+        globals.user = getattr(request, 'user', None)
 
 # retrocompatibility
 class User(Global):
