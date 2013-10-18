9b35bab88677cdceeddea8f51913853f290ffa0f
Alexander Shvetsov <ashvetsov@definitif.ru>
2013-10-18 14:53:13 +0400

    Updated implementation to support executing outside Django context or w/o `Globals` middleware.
    Signed-off-by: Alexander Shvetsov <ashvetsov@definitif.ru>

--- src/django_globals/__init__.py
+++ src/django_globals/__init__.py
@@ -1,2 +1,12 @@
 import threading
 globals = threading.local()
+
+
+# Setting placeholders for `request` and `user`
+# globals to allow calling them outside Django
+# context or w/o globals middleware.
+if not hasattr(globals, 'request'):
+    globals.request = None
+    globals.user = None
+
+    globals.get_current_user = get_current_user
