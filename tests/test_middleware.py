2a7093d3aa2672ec352eb6aaafd2c1bf9a635daa
John Carter <john@therefromhere.org>
2018-03-06 15:11:14 +1300

    Handle case where process_request hasn't been called
    Added test, resolves #22

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
@@ -13,14 +14,14 @@ class TestMiddleware(TestCase):
     """
 
     def test_request(self):
-        response = self.client.get("/assert-request")
+        response = self.client.get("/assert-request/")
 
         self.assertEqual(response.status_code, 200)
 
     def test_anon_user(self):
         self.client.logout()
 
-        response = self.client.get("/assert-user")
+        response = self.client.get("/assert-user/")
 
         self.assertEqual(response.status_code, 200)
 
@@ -28,6 +29,16 @@ class TestMiddleware(TestCase):
         user = User.objects.create_user(username='testuser', email='user@example.com', password='top_secret')
         self.client.login(username='testuser', password='top_secret')
 
-        response = self.client.get("/assert-user")
+        response = self.client.get("/assert-user/")
 
         self.assertEqual(response.status_code, 200)
+
+    def test_redirect(self):
+        # APPEND_SLASH causes a 301 redirect in django.middleware.common.CommonMiddleware
+        # on old-style middleware (Django<1.10, with MIDDLEWARE_CLASSES) this means our process_request
+        # won't be called but process_response will be
+        self.assertTrue(settings.APPEND_SLASH, "This test requires APPEND_SLASH = True")
+
+        response = self.client.get("/assert-request")
+
+        self.assertEqual(response.status_code, 301)
